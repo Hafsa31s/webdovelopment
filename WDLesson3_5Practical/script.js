8464126d4622449299ab5bef1880de0527e15be4
Hafsa31s <hafsa422shujaatt@outlook.com>
2026-01-15 14:00:49 +0000

    upd 3.1

--- WDLesson3_5Practical/script.js
+++ WDLesson3_5Practical/script.js
@@ -3,15 +3,36 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-function recArea(){
+const length = document.getElementById('length');
+const width = document.getElementById('width');
+const areaResult = document.getElementById('area-result');
+const perimeterResult = document.getElementById('perimeter-result');
 
+
+function recArea(){
+ let l = document.getElementById("length")
+ let w = document.getElementById("width")
+  let output = document.getElementById("output")
+let area = parseFloat (l.value) * parseFloat (w.value);
+output.innerHTML = area;
+ 
 }
 
 function recPerimeter(){
-
+  let l = document.getElementById("length")
+ let w = document.getElementById("width")
+  let output = document.getElementById("output")
+let perimeter = parseFloat (l.value) * parseFloat (w.value);
+output.innerHTML = area;
 }
+  
+document
+  .getElementById("calculate-area")
+  .addEventListener("click", recArea);
 
-
+document
+  .getElementById("calculate-perimeter")
+  .addEventListener("click", recPerimeter);
 /* Challenge 4: Complete the area and circumference functions below for the circle.  Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 3.
       2) Perform the necessary calculations
