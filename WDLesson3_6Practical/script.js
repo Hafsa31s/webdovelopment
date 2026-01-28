2d222365818258719ac65fb4d56796828652502c
Hafsa31s <hafsa422shujaatt@outlook.com>
2026-01-28 14:19:22 +0000

    3.7

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,7 +1,33 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
+function avarage () {
+    let g1 = parseFloat(document.getElementById("g1").value);
+    let g2 = parseFloat(document.getElementById("g2").value);
+    let g3 = parseFloat(document.getElementById("g3").value);
+    let output = document.getElementById("output_avarage");
+      let avg = (g1 + g2 + g3) / 3;
 
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
+function calculateSlope() {
+    let x1 = parseFloat(document.getElementById("x1").value);
+    let y1 = parseFloat(document.getElementById("y1").value);
+    let x2 = parseFloat(document.getElementById("x2").value);
+    let y2 = parseFloat(document.getElementById("y2").value);
+    let output = document.getElementById("output_slope");
+}
 
+        let slope = (y2 - y1) / (x2 - x1);
+        output.innerHTML = "Slope of the line: " + slope.toFixed(2);
+    
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
+function calculateBMI() {
+    let weight = parseFloat(document.getElementById("weight").value);
+    let height = parseFloat(document.getElementById("height").value);
+    let output = document.getElementById("output_bmi");
+}
+
+        let bmi = weight / (height * height);
+        output.innerHTML = "Your BMI is: " + bmi.toFixed(2);
+  
--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -3,12 +3,29 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-
+ function calculateDistance() {
+let x1 = parseFloat(document.getElementById("x1").value);
+let y1 = parseFloat(document.getElementById("y1").value);
+let x2 = parseFloat(document.getElementById("x2").value);
+let y2 = parseFloat(document.getElementById("y2").value);
+let output = document.getElementById("output");
+let a = Math.pow(x2 - x1, 2);
+  let b = Math.pow(y2 - y1, 2);
+  let distance = Math.sqrt(a + b);
+  output.innerHTML = "Distance = " + distance;
+}
+ 
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-
-
-
+function calculateCompound() {
+  let P = parseFloat(document.getElementById("principal").value);
+  let r = parseFloat(document.getElementById("rate").value) / 100;
+  let t = parseFloat(document.getElementById("time").value);
+  let n = parseFloat(document.getElementById("compound").value);
+  let output = document.getElementById("compoundOutput");
+  let A = P * Math.pow(1 + r / n, n * t);
+  output.innerHTML = "Total Amount is " + A;
+}
