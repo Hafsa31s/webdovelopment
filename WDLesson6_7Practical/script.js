af7a7a6e2cb486d9a4f451c37a36f979835ee6e1
Hafsa31s <hafsa422shujaatt@outlook.com>
2026-04-30 13:27:11 +0000

    UPD 6.7

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -1,9 +1,9 @@
-let data;
+let data; 
 async function init(){
 
 let link = "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
 let info = await fetch(link);
-let data = await info.json();
+data = await info.json();
 
   let output = document.getElementById("output");
   let build = "";
@@ -13,8 +13,11 @@ for (let i = 0; i < data.length; i++){
    build += `<div class="fitted card">
                  <h3>${collision.on_street_name}</h3>
                  <h4>${collision.off_street_name}</h4>
-                 <p>${collision.crash_date}</p>
-                 <p>${collision.crash_time}</p>
+                 <p>Date: ${collision.crash_date}</p>
+                 <p>Time: ${collision.crash_time}</p>
+                 <p> People injured: ${collision.number_of_persons_injured}</p>
+                 <hr>
+                 <p> Collision_ID: ${collision.collision_id}</p>
                  <hr>
              </div>`;
       ct += 1;
@@ -23,3 +26,55 @@ for (let i = 0; i < data.length; i++){
   output.innerHTML = build;
 }
  
+function filterByTime(){
+  let output = document.getElementById("output");
+  let Time = document.getElementById("Time").value;
+  let result = document.getElementById("result");
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i+=1){
+    let collision = data[i];
+    if(collision.crash_time == Time){
+     build += `<div class="fitted card">
+                 <h3>${collision.on_street_name}</h3>
+                 <h4>${collision.off_street_name}</h4>
+                 <p>Date: ${collision.crash_date}</p>
+                 <p>Time: ${collision.crash_time}</p>
+                 <p> People injured: ${collision.number_of_persons_injured}</p>
+                 <hr>
+                 <p> Collision_ID: ${collision.collision_id}</p>
+                 <hr>
+             </div>`;
+      ct+=1;
+    }
+  }
+    result.innerHTML = `${ct} Results found.`;
+  output.innerHTML = build;
+}
+function filterBystreet(){
+  let output = document.getElementById("output");
+  let street = document.getElementById("street").value;
+  let result = document.getElementById("result");
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i++){
+    let collision = data[i];
+    if(collision.on_street_name ==street){
+     build += `<div class="fitted card">
+                 <h3>${collision.on_street_name}</h3>
+                 <h4>${collision.off_street_name}</h4>
+                 <p>Date: ${collision.crash_date}</p>
+                 <p>Time: ${collision.crash_time}</p>
+                 <p> People injured: ${collision.number_of_persons_injured}</p>
+                 <hr>
+                 <p> Collision_ID: ${collision.collision_id}</p>
+                 <hr>
+             </div>`;
+      ct+=1;
+    }
+  }
+    result.innerHTML = `${ct} Results found.`;
+  output.innerHTML = build;
+}
